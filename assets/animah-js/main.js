197067887ed18e5a11839b974019e4ec4ad45132
Alejandro Piñeiro <infapi00@gmail.com>
2013-05-09 20:02:05 +0200

    UI update

--- assets/animah-js/main.js
+++ assets/animah-js/main.js
@@ -16,3 +16,47 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// Players array, Name, Base, Random, Total
+var players = [ ["Pepe", 45, 0],
+                ["Manolo", 85, 0]];
+
+var player_table = null;
+var myMath = null;
+
+function onDocumentReady () {
+    player_table = $('#player_table').dataTable( { 'bFilter': false,
+                                                   'bInfo': false,
+                                                   'bPaginate': false,
+                                                   'aoColumns': [ null, {'sClass': 'center'}, {'sClass': 'center'}, {'sClass': 'center'}]
+                                                 } );
+    myMath = new AnimahMath();
+    new_round ();
+}
+
+function updateTable () {
+    player_table.fnClearTable();
+    for (i = 0; i < players.length; i++) {
+        player_table.fnAddData(players[i]);
+    }
+}
+
+//FIXME: bad name and/or place
+
+function new_round () {
+
+    for (i = 0; i < players.length; i++) {
+        players[i][2] = myMath.get_dice_roll ();
+        players[i][3] = players[i][1] + players[i][2];
+    }
+
+    updateTable();
+}
+
+// Math section
+function AnimahMath() {
+}
+
+AnimahMath.prototype.get_dice_roll = function(roll_type) {
+    return Math.floor((Math.random()*100)+1)
+};
