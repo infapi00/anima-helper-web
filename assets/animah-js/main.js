aca8eeb6aa3567a0160df4ee4b72c4a7f6de0ae8
Alejandro Piñeiro <infapi00@gmail.com>
2013-05-10 18:52:28 +0200

    Basic app skeleton created
    Skeleton created for all the pages needed, major UI change.
    Now is only about filling holes.

--- assets/animah-js/main.js
+++ assets/animah-js/main.js
@@ -18,39 +18,74 @@
  */
 
 // Players array, Name, Base, Random, Total
-var players = [ ["Pepe", 45, 0],
-                ["Manolo", 85, 0]];
+var players = [ ["Lorenzo", 45, 0, 0],
+                ["Atanasio", 85, 0, 0],
+                ["Zenon", 115, 0, 0],
+                ["Fatima", 75, 0, 0],
+                ["Uriel", 70, 0, 0]];
 
-var player_table = null;
+var player_table;
 var myMath = null;
 
 function onDocumentReady () {
+
+    /* Add a click handler for the delete row */
+    $('#delete').click( function() {
+	var anSelected = fnGetSelected( player_table );
+	if ( anSelected.length !== 0 ) {
+	    player_table.fnDeleteRow( anSelected[0] );
+	}
+    } );
+
     player_table = $('#player_table').dataTable( { 'bFilter': false,
                                                    'bInfo': false,
                                                    'bPaginate': false,
                                                    'aoColumns': [ null, {'sClass': 'center'}, {'sClass': 'center'}, {'sClass': 'center'}]
                                                  } );
+
     myMath = new AnimahMath();
-    new_round ();
+    updateTable ();
+}
+
+/* Get the rows which are currently selected */
+function fnGetSelected( oTableLocal )
+{
+    return oTableLocal.$('tr.row_selected');
 }
 
 function updateTable () {
     player_table.fnClearTable();
     for (i = 0; i < players.length; i++) {
+        players[i][3] = players[i][1] + players[i][2];
         player_table.fnAddData(players[i]);
     }
+
+    player_table.$('tbody tr').click (function( e ) {
+        if ( $(this).hasClass('row_selected') ) {
+	    $(this).removeClass('row_selected');
+        }
+        else {
+	    player_table.$('tr.row_selected').removeClass('row_selected');
+	    $(this).addClass('row_selected');
+        }
+    });
 }
 
+// Callbacks to html elements
 function onNewRound () {
     new_round ();
 }
+
+function onNewHit () {
+    //FIXME: implement this
+}
+
 //FIXME: bad name and/or place
 
 function new_round () {
 
     for (i = 0; i < players.length; i++) {
         players[i][2] = myMath.get_dice_roll ();
-        players[i][3] = players[i][1] + players[i][2];
     }
 
     updateTable();
